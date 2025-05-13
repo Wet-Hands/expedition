5122957d8c50b4d459fbbf7ede5b4f502b01724d
Ian Day <109267942+Wet-Hands@users.noreply.github.com>
2025-05-12 20:42:44 -0400

    firethrower

--- scripts/player/interaction_manager.gd
+++ scripts/player/interaction_manager.gd
@@ -5,8 +5,12 @@ extends Node
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("action1"):
 		if ray.is_colliding():
-			if ray.get_collider().has_method("interact"):
-				print("INTERACTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+			var tree = ray.get_collider()
+			if tree.has_method("interact"):
+				if tree.get_text().contains("TORCH"):
+					$AnimationPlayer.play("fire")
+					await get_tree().create_timer(2.5).timeout
+					tree.set_fire()
 
 func _on_timer_timeout() -> void:
 	if ray.is_colliding():
--- scripts/tree.gd
+++ scripts/tree.gd
@@ -1,10 +1,21 @@
 extends StaticBody3D
 
 var tree_state : int = 0
-var tree_texts = ["TORCH TREE [LMB]", "TORCH TREE [LMB]", "DEAD TREE"]
+var tree_texts = ["TORCH TREE [LMB]", "TORCH TREE [LMB]", "DEAD TREE", "BURNING TREE"]
 
 func interact():
 	pass
 
 func get_text():
 	return tree_texts[tree_state]
+
+func set_fire():
+	$GPUParticles3D.emitting = true
+	tree_state = 3
+	$FireTimer.start()
+
+func _on_fire_timer_timeout() -> void:
+	$GPUParticles3D.emitting = false
+	$tree_5/tree_5_leaves.hide()
+	$FireTimer.stop()
+	tree_state = 2
