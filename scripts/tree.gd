5b881d5b4d58d454faef8b29b608d217dfa2ac9c
Ian Day <ian.day21@gmail.com>
2025-05-13 18:48:48 -0400

    fire bad

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -3,6 +3,15 @@ extends StaticBody3D
 var tree_state : int = 0
 var tree_texts = ["TORCH TREE [LMB]", "TORCH TREE [LMB]", "DEAD TREE", "BURNING TREE"]
 
+func _ready() -> void:
+	refresh()
+
+func refresh():
+	if tree_state == 0:
+		$tree_5/tree_5_leaves.material_override.albedo_color = Color(.5, 1, 0)
+	if tree_state == 1:
+		$tree_5/tree_5_leaves.material_override.albedo_color = Color(3, 2, 0)
+
 func interact():
 	pass
 
@@ -12,6 +21,7 @@ func get_text():
 func set_fire():
 	$GPUParticles3D.emitting = true
 	tree_state = 3
+	$AudioStreamPlayer3D.play()
 	$FireTimer.start()
 
 func _on_fire_timer_timeout() -> void:
